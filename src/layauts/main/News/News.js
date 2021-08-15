df12f9377cab5e95561d06b90965bf27922da5db
Pawel D <dziarmaga8@gmail.com>
2021-08-15 13:45:32 +0200

    Add news-slider

--- src/layauts/main/News/News.js
+++ src/layauts/main/News/News.js
@@ -1,9 +1,81 @@
 import "./News.css";
+import firstNewsPicture from "../../../Image/News/FirstNews.jpg";
+import secondNewsPicture from "../../../Image/News/SecondNews.jpg";
+import thirdNewsPicture from "../../../Image/News/ThirdNews.jpg";
+import { useEffect, useState } from "react";
 
 function News() {
+	const currentSlide = 1;
+	const manualNav = (event) => {
+		const chosenElementClass = event.target.classList[1];
+
+		const slides = document.getElementsByClassName("news__slide");
+		console.log(slides);
+		slides[0].classList.remove("active");
+		slides[1].classList.remove("active");
+		slides[2].classList.remove("active");
+		const btms = document.getElementsByClassName("news__navigation-btn");
+		btms[0].classList.remove("active");
+		btms[1].classList.remove("active");
+		btms[2].classList.remove("active");
+
+		const chosenElement =
+			document.getElementsByClassName(chosenElementClass);
+		chosenElement[0].classList.add("active");
+		chosenElement[1].classList.add("active");
+	};
+
 	return (
 		<div className='news main-element'>
-			<h1>Aktualności</h1>
+			<div className='news__slider '>
+				<div className='news__slide first-news active'>
+					<img src={firstNewsPicture} alt='First news'></img>
+					<div className='news__info'>
+						<h3>UWAGA RUSZAMY!</h3>
+						<p>
+							Już niedługo w Chorzowie powstanie wyjątkowe
+							miejsce! Miejsce pełne pasji, zaangażowania, radości
+							i satysfakcji.
+						</p>
+					</div>
+				</div>
+
+				<div className='news__slide second-news'>
+					<img src={secondNewsPicture} alt='First news'></img>
+					<div className='news__info'>
+						<h3>UWAGA RUSZAMY!</h3>
+						<p>
+							Już niedługo w Chorzowie powstanie wyjątkowe
+							miejsce! Miejsce pełne pasji, zaangażowania, radości
+							i satysfakcji.
+						</p>
+					</div>
+				</div>
+
+				<div className='news__slide third-news'>
+					<img src={thirdNewsPicture} alt='First news'></img>
+					<div className='news__info'>
+						<h3>UWAGA RUSZAMY!</h3>
+						<p>
+							Już niedługo w Chorzowie powstanie wyjątkowe
+							miejsce! Miejsce pełne pasji, zaangażowania, radości
+							i satysfakcji.
+						</p>
+					</div>
+				</div>
+
+				<div className='news__navigation'>
+					<div
+						onClick={manualNav}
+						className='news__navigation-btn first-news active'></div>
+					<div
+						onClick={manualNav}
+						className='news__navigation-btn second-news'></div>
+					<div
+						onClick={manualNav}
+						className='news__navigation-btn third-news'></div>
+				</div>
+			</div>
 		</div>
 	);
 }
