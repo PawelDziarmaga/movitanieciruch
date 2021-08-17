472f3298eeac54334c896932b4d813e3efbe107d
Pawel D <dziarmaga8@gmail.com>
2021-08-17 19:55:43 +0200

    Add fitsey

--- src/Pages/FitnesPage/FitnesPage.js
+++ src/Pages/FitnesPage/FitnesPage.js
@@ -4,6 +4,8 @@ function FitnesPage() {
 	return (
 		<div className='fitnes-page'>
 			<h1>Fitness</h1>
+
+			<lb-schedule-widget category='E55A7B66-3D4E-5ADA-2F70-521A7B374D70'></lb-schedule-widget>
 		</div>
 	);
 }
--- src/layauts/main/Graphic/Graphic.js
+++ src/layauts/main/Graphic/Graphic.js
@@ -1,15 +1,11 @@
 import "./Graphic.css";
-import ImgGraphic from "../../../Image/Graphic/first-hall.jpg";
 
 function Graphic() {
 	return (
 		<div id='idGraphic' className='graphic main-element'>
 			<h1>Grafik</h1>
 			<div className='graphic-link'>
-				<img
-					src={ImgGraphic}
-					alt='first-hall'
-					className='graphicImg graphic__first-hall'></img>
+				<lb-schedule-widget></lb-schedule-widget>
 			</div>
 		</div>
 	);
